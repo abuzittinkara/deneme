79a62c509235ef318329ce6ea2fb2ebc84b3bb4a
abuzittinkara <132690912+abuzittinkara@users.noreply.github.com>
2024-12-05 14:03:59 +0300

    Güncelleme açıklaması

--- public/script.js
+++ public/script.js
@@ -1,38 +1,36 @@
-// public/script.js
 const socket = io();
 let localStream;
 let peers = {};
 
-// Access microphone
+// Mikrofon erişimi al
 navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
   localStream = stream;
 });
 
-// Start call button
-document.getElementById("startCall").addEventListener("click", () => {
-  socket.emit("join");
-});
-
-// Handle signaling
+// Kullanıcı bağlantı kurduğunda
 socket.on("signal", async (data) => {
   const { from, signal } = data;
   let peer;
 
   if (!peers[from]) {
-    peer = new RTCPeerConnection();
+    peer = new RTCPeerConnection({
+      iceServers: [
+        { urls: "stun:stun.l.google.com:19302" }, // STUN sunucusu
+      ],
+    });
     peers[from] = peer;
 
-    // Add local stream tracks to peer
+    // Mikrofon stream'ini ekle
     localStream.getTracks().forEach((track) => peer.addTrack(track, localStream));
 
-    // Send signaling data back to the other peer
+    // ICE adaylarını gönder
     peer.onicecandidate = (event) => {
       if (event.candidate) {
         socket.emit("signal", { to: from, signal: event.candidate });
       }
     };
 
-    // Play remote audio stream
+    // Ses stream'i geldiğinde çal
     peer.ontrack = (event) => {
       const audio = new Audio();
       audio.srcObject = event.streams[0];
