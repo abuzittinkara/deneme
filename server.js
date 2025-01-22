6d38c14255fd216c3b19437e0375a63f54c85ef2
Abuzittin Kara <mg2001trikol61@gmaail.com>
2025-01-22 22:36:00 +0300

    MongoDB bağlantı URI güncellendi; kullanıcı adı değiştirildi.

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const server = http.createServer(app);
 const io = socketIO(server);
 
 // MongoDB bağlantısı
-const uri = process.env.MONGODB_URI || "mongodb+srv://username:password@cluster0.example.mongodb.net/myappdb?retryWrites=true&w=majority";
+const uri = process.env.MONGODB_URI || "mongodb+srv://abuzorttin:HWZe7uK5yEAE@cluster0.vdrdy.mongodb.net/myappdb?retryWrites=true&w=majority";
 mongoose.connect(uri)
   .then(() => console.log("MongoDB bağlantısı başarılı!"))
   .catch(err => console.error("MongoDB bağlantı hatası:", err));
