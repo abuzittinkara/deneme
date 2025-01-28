4e219b41990baa6b317c97398f85e4dd47423307
Abuzittin Kara <mg2001trikol61@gmaail.com>
2025-01-28 22:31:19 +0300

    mediasoup-client için lisans dosyası eklendi; webpack yapılandırmasına açıklamalar eklendi.

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,10 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
+  // Giriş (entry) => node_modules/mediasoup-client/lib/index.js
   entry: path.resolve(__dirname, 'node_modules', 'mediasoup-client', 'lib', 'index.js'),
+
+  // Çıkış (output) => public/libs/mediasoup-client.min.js
   output: {
     path: path.resolve(__dirname, 'public', 'libs'),
     filename: 'mediasoup-client.min.js'
