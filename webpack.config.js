38f9733908b98b1bbec486ca8be8ce597a995862
Abuzittin Kara <mg2001trikol61@gmaail.com>
2025-01-28 22:50:40 +0300

    webpack yapılandırmasına global değişken ayarları eklendi; 'mediasoupClient' için library ve libraryTarget tanımlandı.

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,12 @@ module.exports = {
   // Çıkış (output) => public/libs/mediasoup-client.min.js
   output: {
     path: path.resolve(__dirname, 'public', 'libs'),
-    filename: 'mediasoup-client.min.js'
+    filename: 'mediasoup-client.min.js',
+
+    // Burada eklediğimiz ayarlar:
+    // library: 'mediasoupClient' => "mediasoupClient" isminde global değişken yaratır
+    // libraryTarget: 'window'    => Bu global değişkeni window.mediasoupClient olarak ekler
+    library: 'mediasoupClient',
+    libraryTarget: 'window'
   }
 };
