5f158b87e7c1e807e84baaf87b9c5803ce559928
Abuzittin Kara <mg2001trikol61@gmaail.com>
2025-01-29 20:01:56 +0300

    "createWebRtcTransport" fonksiyonunda STUN sunucusu güncellendi.

--- sfu.js
+++ sfu.js
@@ -100,7 +100,7 @@ async function createWebRtcTransport(router) {
      */
     iceServers: [
       {
-        urls: 'stun:stun.relay.metered.ca:80'
+        urls: 'stun:stun.l.google.com:19302'
       },
       {
         urls: 'turn:global.relay.metered.ca:80',
