7e79ed8a948d63b11c133883d6474a99c6a43c9b
Abuzittin Kara <mg2001trikol61@gmail.com>
2025-04-03 15:57:27 -0700

    socket.io-client bağımlılığı eklendi; React 18 için render yöntemi güncellendi.

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import { SocketProvider } from './content/SocketContext';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
-    <SocketProvider>
-      <App />
-    </SocketProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
