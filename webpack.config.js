95bf9bfb6ced753e673f9bc2b4be0c5264734e1a
Abuzittin Kara <mg2001trikol61@gmaail.com>
2025-04-04 00:58:57 +0300

    Webpack yapılandırması güncellendi; giriş dosyası değiştirildi ve çıkış yolu ile dosya adı güncellendi. Babel ve CSS yükleyicileri eklendi, kaynak haritaları etkinleştirildi.

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,33 @@
-// webpack.config.js
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
-  // Giriş (entry) => node_modules/mediasoup-client/lib/index.js
-  entry: path.resolve(__dirname, 'node_modules', 'mediasoup-client', 'lib', 'index.js'),
-
-  // Çıkış (output) => public/libs/mediasoup-client.min.js
+  mode: 'production', // veya development, ihtiyaca göre ayarlayın
+  entry: path.resolve(__dirname, 'client', 'src', 'index.js'),
   output: {
-    path: path.resolve(__dirname, 'public', 'libs'),
-    filename: 'mediasoup-client.min.js',
-
-    // Burada eklediğimiz ayarlar:
-    // library: 'mediasoupClient' => "mediasoupClient" isminde global değişken yaratır
-    // libraryTarget: 'window'    => Bu global değişkeni window.mediasoupClient olarak ekler
-    library: 'mediasoupClient',
-    libraryTarget: 'window'
-  }
+    path: path.resolve(__dirname, 'client', 'public', 'js'),
+    filename: 'bundle.js',
+    publicPath: '/js/'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  devtool: 'source-map'
 };
