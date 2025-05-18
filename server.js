705301a905749586c1ca5927aadeb5f2cc52dca9
Abuzittin Kara <mg2001trikol61@gmail.com>
2025-05-19 02:04:10 +0300

    fix: dotenv yapılandırmasını başa taşı ve gereksiz boşlukları kaldır

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-  
- const express = require('express');
+require('dotenv').config();
+
+const express = require('express');
  const socketIO = require('socket.io');
  const WebSocket = require('ws');
  const http = require('http');
