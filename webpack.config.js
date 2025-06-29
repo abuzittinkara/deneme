af79eb2eca540858ef6818f0f7881a661706810d
Abuzittin Kara <132690912+abuzittinkara@users.noreply.github.com>
2025-06-29 04:55:34 +0300

    Add Babel loader setup

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,20 @@ const clientBundleConfig = {
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
   }
 };
 
